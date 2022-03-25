491e41347cb584677a839b48539f8e9654eebee7
Nilofar Mujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-03-25 23:27:56 +0530

    Added: Added: Reading and writing data from/to JSON File UC15.

--- src/com/day28/AddressBookSystem.java
+++ src/com/day28/AddressBookSystem.java
@@ -1,11 +1,25 @@
 package com.day28;
 /**
- *  UC14 :- Ability to Read/Write the Address Book with Persons Contact as CSV File
- *          - Use OpenCSV Library
- *  UC15 :- Ability to Read or Write the Address Book with Persons Contact as JSON File
- *          - Use GSON Library
- *
+ * UC1 :- Ability to create a Contacts in Address Book with first and last names, address,
+ *        city, state, zip, phone number and email...
+ * UC2 :- Ability to add a new Contact to Address Book
+ * UC3 :- Ability to edit existing contact person using their name
+ * UC4 :- Ability to delete a person using person's name
+ * UC5 :- Ability to add multiple person to Address Book
+ * UC6 :- Refactor to add multiple Address Book to the System Each Address Book has a unique Name
+ * UC7 :- Ability to ensure there is no Duplicate Entry of the same Person in a particular Address Book
+ * UC8 :- Ability to search Person in a City or State across the multiple AddressBook
+ * UC9 :- Ability to view Persons by City or State
+ * UC10 :- Ability to get number of contact persons i.e. count by City or State
+ * UC11 :- Ability to sort the entries in the address book alphabetically by Person’s name
+ * UC12 :- Ability to sort the entries in the address book by City,State, or Zip
+ * UC13 :- Ability to Read or Write the Address Book with  Persons Contact into a File using File IO
+ * UC14 :- Ability to Read/Write the Address Book with Persons Contact as CSV File
+ *         - Use OpenCSV Library
+ * UC15 :- Ability to Read or Write the Address Book with Persons Contact as JSON File
+ *         - Use GSON Library
  */
+
 
 /**
  * import IOException class
